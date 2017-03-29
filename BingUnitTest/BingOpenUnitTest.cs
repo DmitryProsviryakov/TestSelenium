39b4d9a39c34001b396551997680d96a7704dfaf
Dmitry Barkalov (Akvelon) <v-dmbark@microsoft.com>
2017-03-29 13:22:50 +0300

    Initial commit

---
+++ BingUnitTest/BingOpenUnitTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+using NUnit.Framework;
+
+namespace BingUnitTest
+{
+	/// <summary>
+	/// Test class
+	/// </summary>
+	/// <typeparam name="TWebDriver">Test parameter - browser's driver</typeparam>
+	[TestFixture(typeof(FirefoxDriver))]
+	[TestFixture(typeof(ChromeDriver))]
+	public class BingOpenUnitTest<TWebDriver> where TWebDriver : IWebDriver, new()
+	{
+		/// <summary>
+		/// Browser's driver
+		/// </summary>
+		IWebDriver _driver;
+
+		/// <summary>
+		/// Object of Bing page object model
+		/// </summary>
+		BingPageObjectModel bing;		
+
+		/// <summary>
+		/// Initialization befor tests
+		/// Initialize browser's driver
+		/// Open bing.com
+		/// Search "twitter" in bing
+		/// </summary>
+		[TestFixtureSetUp]
+		public void TestSetup()
+		{
+			_driver = new TWebDriver();
+			_driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(5);
+			bing = new BingPageObjectModel(_driver);
+			bing.OpenBing();
+			bing.Search("twitter");
+		}
+
+		/// <summary>
+		/// Test for search result
+		/// </summary>
+		[Test]
+		public void TestGetFirstLink()
+		{
+			string actualResult = bing.GetResultLinks().First().GetAttribute("href");
+
+			Assert.IsNotEmpty(actualResult);
+		}
+
+		/// <summary>
+		/// Close browser after test
+		/// </summary>
+		[TestFixtureTearDown]
+		public void TearDown()
+		{
+			_driver.Dispose();
+		}
+	}
+}
---
+++ BingUnitTest/BingPageObjectModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace BingUnitTest
+{
+	/// <summary>
+	/// Page Object model for Bing.com page
+	/// </summary>
+	public class BingPageObjectModel
+	{
+		/// <summary>
+		/// Browser driver
+		/// </summary>
+		IWebDriver driver;
+		
+		/// <summary>
+		/// Element for search textbox input
+		/// </summary>
+		IWebElement searchBox;
+
+		/// <summary>
+		/// Element for search button input
+		/// </summary>
+		IWebElement searchButton;
+		
+		/// <summary>
+		/// Locator for search textbox input
+		/// </summary>
+		By searchBoxLocator = By.Id("sb_form_q");
+		
+		/// <summary>
+		/// Locator for search button input
+		/// </summary>
+		By searchButtonLocator = By.Id("sb_form_go");
+
+		/// <summary>
+		/// Locator for link's in search result page
+		/// </summary>
+		By resultLinkLocator = By.CssSelector("#b_results .b_title a");
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="_driver">Reference to driver of test browser</param>
+		public BingPageObjectModel(IWebDriver _driver)
+		{
+			driver = _driver;
+		}
+
+		/// <summary>
+		/// Method for open page "http://www.bing.com/" and initialize defaultt bing elements,
+		/// which displayd on all bing pages
+		/// </summary>
+		public void OpenBing()
+		{
+			driver.Url = "http://www.bing.com/";
+			searchBox = driver.FindElement(searchBoxLocator);
+			searchButton = driver.FindElement(searchButtonLocator);
+		}
+
+		/// <summary>
+		/// Method for set some string to search textbox and find information about this in bing
+		/// </summary>
+		/// <param name="query">search query</param>
+		public void Search(string query)
+		{
+			searchBox.SendKeys(query);
+
+			searchButton.Click();
+		}
+
+		/// <summary>
+		/// Method for find all result links in the page
+		/// </summary>
+		/// <returns>List of web elements</returns>
+		public IEnumerable<IWebElement> GetResultLinks()
+		{
+			return driver.FindElements(resultLinkLocator);
+		}
+
+		/// <summary>
+		/// Method for close browser and is's driver
+		/// </summary>
+		public void Close()
+		{
+			driver.Dispose();
+		}
+	}
+}
